b01ac99be0e2dc28ccf11734de348f9ac130755a
{yuji-nakaoka} <{yu.creative.131@gmail.com}>
2022-05-02 10:17:53 +0900

    freezd上手くいった

---
+++ lib/models/item_model.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'item_model.freezed.dart';
+part 'item_model.g.dart';
+
+@freezed
+class Item with _$Item {
+  const factory Item({
+     String? id,
+    required String name,
+    @Default(false) bool obtained,
+  }) = _Item;
+
+  factory Item.fromJson(Map<String, dynamic> json) =>
+      _$ItemFromJson(json);
+}
