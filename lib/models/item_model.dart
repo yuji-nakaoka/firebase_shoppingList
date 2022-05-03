13940f056f5951bf918ef13dbb8b2cab947f2a97
{yuji-nakaoka} <{yu.creative.131@gmail.com}>
2022-05-03 16:08:13 +0900

    import

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 
@@ -7,11 +10,19 @@ part 'item_model.g.dart';
 @freezed
 class Item with _$Item {
   const factory Item({
-     String? id,
+    String? id,
     required String name,
     @Default(false) bool obtained,
   }) = _Item;
 
-  factory Item.fromJson(Map<String, dynamic> json) =>
-      _$ItemFromJson(json);
+  factory Item.empty() => Item(name: '');
+
+  factory Item.fromJson(Map<String, dynamic> json) => _$ItemFromJson(json);
+
+  factory Item.fromDocument(DocumentSnapshot doc) {
+    final data = doc.data()! as Map<String, dynamic>;
+    return Item.fromJson(data).copyWith(id: doc.id);
+  }
+
+  Map<String, dynamic> toDocument() => toJson()..remove('id');
 }
